ad75a7086f91bd0744bb8525c99a666f684d8d8a
allisonnault <allisonrnault@gmail.com>
2023-02-18 10:03:48 -0600

    prompt working

---
+++ index.js
@@ -0,0 +1,21 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions')
+const questionInfo = [];
+
+
+function init () {
+    return inquirer
+    .prompt(questions)
+   
+}
+
+init()
+.then((response)=>
+{questionInfo.push(response)
+
+// fs.writeFile(`${response.text}.svg`, generate function)
+// still need to finsih this function!)
+})
+
+
