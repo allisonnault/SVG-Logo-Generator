02ffcf565a0b4f2e1d594d111f11834656faadb1
allisonnault <allisonrnault@gmail.com>
2023-02-20 09:47:29 -0600

    modified shapes and svg

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions');
+const SVG = require('./lib/svg')
 const { Square, Triangle, Circle } = require("./lib/shapes");
-const questionInfo = [];
+
 
 
 inquirer
     .prompt(questions)
     .then((response) => {
-        questionInfo.push(response)
-
+   
         fs.writeFile(`${response.text}.svg`, genSVG(response), (err) => {
             if (err) throw err;
             console.log("Your new SVG file was created!");
@@ -19,16 +19,25 @@ inquirer
 
 function genSVG(response) {
     if (response.shape === "square") {
-        const newSVG = new Square(response.text, response.color, response.shape, response.shapeColor);
-        newSVG.setColor();
-        return newSVG.render();
+        const svg= new SVG();
+        svg.setText(response.text, response.color);
+        const newSquare = new Square();
+        newSquare.setColor(response.shapeColor);
+        svg.setShape(newSquare);
+        return svg.render();
     } else if (response.shape === "triangle") {
-        const newSVG = new Triangle(response.text, response.color, response.shape, response.shapeColor);
-        newSVG.setColor();
-        return newSVG.render();
+        const svg= new SVG();
+        svg.setText(response.text, response.color);
+        const newTriangle = new Triangle();
+        newTriangle.setColor(response.shapeColor);
+        svg.setShape(newTriangle);
+        return svg.render();
     } else {
-        const newSVG = new Circle(response.text, response.color, response.shape, response.shapeColor);
-        newSVG.setColor();
-        return newSVG.render();
+        const svg= new SVG();
+        svg.setText(response.text, response.color);
+        const newCircle = new Circle();
+        newCircle.setColor(response.shapeColor);
+        svg.setShape(newCircle);
+        return svg.render();
     }
 }
