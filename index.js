39f947476534c0b439422adb9b88c7fdd5476a4a
allisonnault <allisonrnault@gmail.com>
2023-02-20 16:12:45 -0600

    final edits

--- index.js
+++ index.js
@@ -19,25 +19,25 @@ inquirer
 
 function genSVG(response) {
     if (response.shape === "square") {
-        const svg= new SVG();
-        svg.setText(response.text, response.color);
+        const logo = new SVG();
+        logo.setText(response.text, response.color);
         const newSquare = new Square();
         newSquare.setColor(response.shapeColor);
-        svg.setShape(newSquare);
-        return svg.render();
+        logo.setShape(newSquare);
+        return logo.render();
     } else if (response.shape === "triangle") {
-        const svg= new SVG();
-        svg.setText(response.text, response.color);
+        const logo = new SVG();
+        logo.setText(response.text, response.color);
         const newTriangle = new Triangle();
         newTriangle.setColor(response.shapeColor);
-        svg.setShape(newTriangle);
-        return svg.render();
+        logo.setShape(newTriangle);
+        return logo.render();
     } else {
-        const svg= new SVG();
-        svg.setText(response.text, response.color);
+        const logo = new SVG();
+        logo.setText(response.text, response.color);
         const newCircle = new Circle();
         newCircle.setColor(response.shapeColor);
-        svg.setShape(newCircle);
-        return svg.render();
+        logo.setShape(newCircle);
+        return logo.render();
     }
 }
